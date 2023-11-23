c709d33892fdbcc6c7dc03e594313765859a4b48
Atul B Raj <97410226+atulbraj@users.noreply.github.com>
2023-11-23 21:03:45 +0530

    QR_generator

---
+++ qr.py
@@ -0,0 +1,3 @@
+import qrcode as qr
+img=qr.make("https://indianmemetemplates.com/wp-content/uploads/phir-hera-pheri-paisa-hi-paisa-hoga-meme-template-1024x535.jpg?is-pending-load=1")
+img.save("myfirst_qr.png")
